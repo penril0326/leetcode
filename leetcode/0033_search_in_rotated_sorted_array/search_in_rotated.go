76248613ce106c68a66bbdcece355104a8d645cd
michaelwang <shaoyungwong@gmail.com>
2021-05-13 23:38:35 +0800

    implement solution of leetcode 33

---
+++ leetcode/0033_search_in_rotated_sorted_array/search_in_rotated.go
@@ -0,0 +1,29 @@
+package search_in_rotated
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if target == nums[mid] {
+			return mid
+		}
+
+		if nums[mid] < nums[right] {
+			if (target > nums[mid]) && (target <= nums[right]) {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if (target >= nums[left]) && (target < nums[mid]) {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
