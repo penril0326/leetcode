7511c603b1f6f0e8a7b95bba5d6ceb1be4f3f3eb
michaelwang <shaoyungwong@gmail.com>
2021-05-17 23:46:56 +0800

    add solution of 49 without sort string

--- leetcode/0049_group_anagrams/group_anagrams.go
+++ leetcode/0049_group_anagrams/group_anagrams.go
@@ -1,11 +1,10 @@
 package group_anagrams
 
-import (
-	"sort"
-)
+import "sort"
 
 type runeList []rune
 
+// solution 1: use sort
 func groupAnagrams(strs []string) [][]string {
 	wordMap := map[string][]string{}
 	for _, str := range strs {
@@ -35,4 +34,35 @@ func (r runeList) Less(i, j int) bool {
 
 func (r runeList) Swap(i, j int) {
 	r[i], r[j] = r[j], r[i]
+}
+
+// solution 2: without sort
+func groupAnagrams2(strs []string) [][]string {
+	wordMap := map[string][]string{}
+	for _, str := range strs {
+		digitCountList := make([]int, 26)
+		for _, c := range str {
+			digitCountList[c-'a']++
+		}
+
+		wordKey := ""
+		for i := 0; i < 26; i++ {
+			if 0 == digitCountList[i] {
+				continue
+			}
+
+			wordKey = wordKey + string(rune(i+'a')) + string(rune(digitCountList[i]))
+		}
+
+		strList := wordMap[wordKey]
+		strList = append(strList, str)
+		wordMap[wordKey] = strList
+	}
+
+	result := [][]string{}
+	for _, strList := range wordMap {
+		result = append(result, strList)
+	}
+
+	return result
 }
