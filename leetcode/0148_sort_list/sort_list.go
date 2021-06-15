1b1362435ff5dd3a5c72044c26afb744a3ee479e
michaelwang <shaoyungwong@gmail.com>
2021-06-15 23:04:44 +0800

    Implement leetcode solution of 148

---
+++ leetcode/0148_sort_list/sort_list.go
@@ -0,0 +1,48 @@
+package sort_list
+
+import "practice/leetcode"
+
+func sortList(head *leetcode.ListNode) *leetcode.ListNode {
+	if (head == nil) || (head.Next == nil) {
+		return head
+	}
+
+	slow, fast, previous := head, head, head
+	for (fast != nil) && (fast.Next != nil) {
+		previous = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	previous.Next = nil
+	return merge(sortList(head), sortList(slow))
+}
+
+func merge(left, right *leetcode.ListNode) *leetcode.ListNode {
+	head := &leetcode.ListNode{
+		Val: -1,
+	}
+
+	current := head
+	for (left != nil) && (right != nil) {
+		if left.Val < right.Val {
+			current.Next = left
+			left = left.Next
+		} else {
+			current.Next = right
+			right = right.Next
+		}
+
+		current = current.Next
+	}
+
+	if left != nil {
+		current.Next = left
+	}
+
+	if right != nil {
+		current.Next = right
+	}
+
+	return head.Next
+}
