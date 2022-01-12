f88acbd45c58de57cabd31a0d1844fa3095992cc
penril0326 <penril0326@gmail.com>
2022-01-12 13:08:55 +0800

    implement leetcode 35

---
+++ leetcode/0035_search_insert_position/search_insert_position.go
@@ -0,0 +1,29 @@
+package searchinsertposition
+
+func searchInsert(nums []int, target int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+
+	// if target bigger than last element just insert to the end
+	if target > nums[length-1] {
+		return length
+	}
+
+	// binary search
+	low, high := 0, length-1
+	for low < high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid
+		} else {
+			low = mid + 1
+		}
+	}
+
+	// finally low is equal high so is ok to return one of them
+	return high
+}
