25d39bdc9409bf471d19c4d4a91c32fb5c7a2101
penril0326 <penril0326@gmail.com>
2022-09-16 00:16:41 +0800

    implement 234

--- leetcode/e0234_palindrome_linked_list/palindrome_linked_list.go
+++ leetcode/e0234_palindrome_linked_list/palindrome_linked_list.go
@@ -3,6 +3,53 @@ package palindromelinkedlist
 import "practice/leetcode"
 
 // Time complexity: O(n)
+// Space complexity: O(1)
+func isPalindromeSplit(head *leetcode.ListNode) bool {
+	if head == nil {
+		return true
+	}
+
+	firstHalfEnd := findHalf(head)
+	secondHalfStart := reverseLinkedList(firstHalfEnd.Next)
+
+	p1, p2 := head, secondHalfStart
+	for p2 != nil {
+		if p1.Val != p2.Val {
+			return false
+		}
+
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+
+	firstHalfEnd.Next = reverseLinkedList(secondHalfStart)
+	return true
+}
+
+func reverseLinkedList(head *leetcode.ListNode) *leetcode.ListNode {
+	var prev *leetcode.ListNode
+	for head != nil {
+		cur := head
+		head = head.Next
+		cur.Next = prev
+		prev = cur
+	}
+
+	return prev
+}
+
+func findHalf(head *leetcode.ListNode) *leetcode.ListNode {
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
+
+// -----------------------------
+// Time complexity: O(n)
 // Space complexity: O(n)
 var front *leetcode.ListNode
 
