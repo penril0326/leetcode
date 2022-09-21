c845c9cf268f033ae76e114e0496770198f01588
penril0326 <penril0326@gmail.com>
2022-09-21 22:19:43 +0800

    add 844 O(1) space solution

--- leetcode/e0844_backspace_string_compare/backspace_string_compare.go
+++ leetcode/e0844_backspace_string_compare/backspace_string_compare.go
@@ -86,3 +86,45 @@ func handleBackspace2(s string) string {
 
 	return newStr
 }
+
+// -----------------------------
+// Time complexity: O(m+n), m and n are length of s and t respectively
+// Space complexity: O(1)
+
+func backspaceCompareTwoPointers(s string, t string) bool {
+	iS, skipS := len(s)-1, 0
+	iT, skipT := len(t)-1, 0
+	for (iS >= 0) || (iT >= 0) {
+		iS, skipS = findSubStringIdx(s, iS, skipS)
+		iT, skipT = findSubStringIdx(t, iT, skipT)
+
+		if (iS >= 0) && (iT >= 0) && (s[iS] != t[iT]) {
+			return false
+		}
+
+		if (iS >= 0) != (iT >= 0) {
+			return false
+		}
+
+		iS--
+		iT--
+	}
+
+	return true
+}
+
+func findSubStringIdx(s string, idx, skip int) (int, int) {
+	for idx >= 0 {
+		if s[idx] == '#' {
+			skip++
+			idx--
+		} else if skip > 0 {
+			skip--
+			idx--
+		} else {
+			break
+		}
+	}
+
+	return idx, skip
+}
--- leetcode/e0844_backspace_string_compare/backspace_string_compare_test.go
+++ leetcode/e0844_backspace_string_compare/backspace_string_compare_test.go
@@ -107,3 +107,55 @@ func Test_backspaceCompareWithoutStack(t *testing.T) {
 		})
 	}
 }
+
+func Test_backspaceCompareTwoPointers(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "1",
+			args: args{
+				s: "ab#c",
+				t: "ad#c",
+			},
+			want: true,
+		},
+		{
+			name: "2",
+			args: args{
+				s: "ab##",
+				t: "c#d#",
+			},
+			want: true,
+		},
+		{
+			name: "3",
+			args: args{
+				s: "a###",
+				t: "####",
+			},
+			want: true,
+		},
+		{
+			name: "4",
+			args: args{
+				s: "bdc#",
+				t: "baaad####d",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := backspaceCompareTwoPointers(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("backspaceCompareTwoPointers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
