27de670ac116d6af53edab18dd9cb0c8e854c729
penril0326 <penril0326@gmail.com>
2022-10-07 01:24:26 +0800

    implement stack

---
+++ leetcode/data_structure.go
@@ -0,0 +1,39 @@
+package leetcode
+
+type myStack struct {
+	stack []interface{}
+}
+
+func NewStack() myStack {
+	return myStack{
+		stack: make([]interface{}, 0),
+	}
+}
+
+func (s myStack) IsEmpty() bool {
+	return len(s.stack) == 0
+}
+
+func (s myStack) Top() interface{} {
+	if !s.IsEmpty() {
+		return s.stack[len(s.stack)-1]
+	}
+
+	return nil
+}
+
+func (s *myStack) Push(node interface{}) {
+	if s != nil {
+		s.stack = append(s.stack, node)
+	}
+}
+
+func (s *myStack) Pop() interface{} {
+	if (s != nil) && !s.IsEmpty() {
+		top := s.Top()
+		s.stack = s.stack[:len(s.stack)-1]
+		return top
+	}
+
+	return nil
+}
