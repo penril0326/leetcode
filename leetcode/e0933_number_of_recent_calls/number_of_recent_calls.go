83302374390cce7a2039422202f72074e2b1d6d4
Michael Wang <michael.wang@vertiv.com>
2022-10-24 15:44:21 +0800

    implement 933

--- leetcode/e0933_number_of_recent_calls/number_of_recent_calls.go
+++ leetcode/e0933_number_of_recent_calls/number_of_recent_calls.go
@@ -1,12 +1,26 @@
 package numberofrecentcalls
 
+import datastructure "practice/data_structure"
+
+// Time complexity: O(1)
+// Space complexity: O(1)
 type RecentCounter struct {
+	que datastructure.MyQueue
 }
 
 func Constructor() RecentCounter {
-	return RecentCounter{}
+	return RecentCounter{
+		que: datastructure.NewQue(),
+	}
 }
 
 func (this *RecentCounter) Ping(t int) int {
-	return 0
+	this.que.Enqueue(t)
+
+	min := t - 3000
+	for !this.que.IsEmpty() && (this.que.Front().(int) < min) { // type assert will decrease performance
+		this.que.Dequeue()
+	}
+
+	return this.que.Length()
 }
