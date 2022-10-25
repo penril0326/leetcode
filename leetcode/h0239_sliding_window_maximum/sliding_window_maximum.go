265412dfa057187ba2bbae558f3b01d25ee70878
penril0326 <penril0326@gmail.com>
2022-10-25 23:30:15 +0800

    add 239 deque solution

--- leetcode/h0239_sliding_window_maximum/sliding_window_maximum.go
+++ leetcode/h0239_sliding_window_maximum/sliding_window_maximum.go
@@ -1,5 +1,28 @@
 package slidingwindowmaximum
 
-func maxSlidingWindow(nums []int, k int) []int {
+import "practice/data_structure/queue"
 
+// Time complexity: O(n)
+// Space complexity: O(n)
+func maxSlidingWindow(nums []int, k int) []int {
+	de := queue.NewDeque()
+	result := make([]int, len(nums)-k+1)
+
+	for i := 0; i < len(nums); i++ {
+		for !de.IsEmpty() && (nums[de.Back().(int)] < nums[i]) {
+			de.PopBack()
+		}
+
+		de.PushBack(i)
+
+		if (i - de.Front().(int)) == k {
+			de.PopFront()
+		}
+
+		if i >= (k - 1) {
+			result[i-k+1] = nums[de.Front().(int)]
+		}
+	}
+
+	return result
 }
