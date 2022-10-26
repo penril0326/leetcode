3cec73a863386680c2136352630dd5cb108ab421
Michael Wang <michael.wang@vertiv.com>
2022-10-26 10:25:42 +0800

    modified stack test

--- data_structure/stack/stack_test.go
+++ data_structure/stack/stack_test.go
@@ -1,9 +1,23 @@
 package stack
 
 import (
+	"log"
 	"reflect"
 	"testing"
 )
+
+func TestNewStack(t *testing.T) {
+	stk := NewStack()
+	stk.Push(1)
+	stk.Push(2)
+	assert(stk.Top(), 2)
+	assert(stk.Pop(), 2)
+	assert(stk.IsEmpty(), false)
+
+	assert(stk.Top(), 1)
+	assert(stk.Pop(), 1)
+	assert(stk.IsEmpty(), true)
+}
 
 func TestMyStack_Top(t *testing.T) {
 	tests := []struct {
@@ -56,4 +70,12 @@ func TestMyStack_Pop(t *testing.T) {
 			}
 		})
 	}
+}
+
+func assert(src, target interface{}) bool {
+	if !reflect.DeepEqual(src, target) {
+		log.Fatalf("src = %v, target = %v\n", src, target)
+	}
+
+	return true
 }
