c87d9b9ea4cc84d6c38559a0582d64b0d6c9b970
Michael Wang <michael.wang@vertiv.com>
2022-10-28 18:08:00 +0800

    Optimize 496 brute force solution and add stack solution

--- leetcode/e0496_next_greater_element_1/next_greater_element_1.go
+++ leetcode/e0496_next_greater_element_1/next_greater_element_1.go
@@ -1,28 +1,59 @@
 package nextgreaterelement1
 
-func nextGreaterElement(nums1 []int, nums2 []int) []int {
-	indexTable := make(map[int]int)
-	for i := 0; i < len(nums1); i++ {
-		for j := 0; j < len(nums2); j++ {
-			if nums1[i] == nums2[j] {
-				indexTable[nums1[i]] = j
-				break
+import "practice/data_structure/stack"
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+func nextGreaterElementBruteForce(nums1 []int, nums2 []int) []int {
+	result := make([]int, len(nums1))
+	for idx1, n1 := range nums1 {
+		result[idx1] = -1
+		findNums := false
+		for _, n2 := range nums2 {
+			if n1 == n2 {
+				findNums = true
+			}
+
+			if findNums {
+				if n2 > n1 {
+					result[idx1] = n2
+					findNums = false
+					break
+				}
 			}
 		}
 	}
 
-	result := make([]int, 0)
-	for i := 0; i < len(nums1); i++ {
-		index := indexTable[nums1[i]]
-		nexGreater := -1
-		for j := index + 1; j < len(nums2); j++ {
-			if nums1[i] < nums2[j] {
-				nexGreater = nums2[j]
-				break
+	return result
+}
+
+// Monotonic stack
+// Time complexity: O(n)
+// Space complexity: O(n)
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	hash := make(map[int]int)
+	for idx, v := range nums1 {
+		hash[v] = idx
+	}
+
+	decreasing := stack.NewStack()
+	result := make([]int, len(nums1))
+	for _, v := range nums2 {
+		for !decreasing.IsEmpty() && (decreasing.Top().(int) < v) {
+			if index, isExist := hash[decreasing.Top().(int)]; isExist {
+				result[index] = v
 			}
+
+			decreasing.Pop()
 		}
 
-		result = append(result, nexGreater)
+		decreasing.Push(v)
+	}
+
+	for !decreasing.IsEmpty() {
+		if index, isExist := hash[decreasing.Pop().(int)]; isExist {
+			result[index] = -1
+		}
 	}
 
 	return result
