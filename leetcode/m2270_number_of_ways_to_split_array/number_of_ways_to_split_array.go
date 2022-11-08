48501cac276023425dcb66c48fbcc4bd30eb44c9
Michael Wang <michael.wang@vertiv.com>
2022-11-08 10:38:01 +0800

    add optimized space prefix sum solution of 2270

--- leetcode/m2270_number_of_ways_to_split_array/number_of_ways_to_split_array.go
+++ leetcode/m2270_number_of_ways_to_split_array/number_of_ways_to_split_array.go
@@ -22,3 +22,26 @@ func waysToSplitArray(nums []int) int {
 
 	return count
 }
+
+// Time complexity: O(N)
+// Space complexity: O(1)
+func waysToSplitArrayOptimized(nums []int) int {
+	if len(nums) <= 0 {
+		return 0
+	}
+
+	leftSum, rightSum := 0, 0
+	for _, v := range nums {
+		rightSum += v
+	}
+
+	count, leftSum := 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		leftSum += nums[i]
+		if leftSum >= rightSum-leftSum {
+			count++
+		}
+	}
+
+	return count
+}
