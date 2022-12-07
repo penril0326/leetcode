5e96b7d07d356e82e1578eb3042dbd524fed6849
Michael Wang <michael.wang@vertiv.com>
2022-12-07 17:14:05 +0800

    add optimized solution of 3

--- leetcode/m0003_longest_substring_without_repeating_chars/longest_substring_without_repeating.go
+++ leetcode/m0003_longest_substring_without_repeating_chars/longest_substring_without_repeating.go
@@ -22,3 +22,20 @@ func lengthOfLongestSubstring(s string) int {
 
 	return ans
 }
+
+// Time complexity: O(N)
+// Space complexity: O(N)
+func lengthOfLongestSubstringOptimized(s string) int {
+	ans := 0
+	count := make(map[byte]int)
+	for left, right := 0, 0; right < len(s); right++ {
+		if index, exist := count[s[right]]; exist && index >= left {
+			left = index + 1
+		}
+
+		ans = math.Max(ans, right-left+1)
+		count[s[right]] = right
+	}
+
+	return ans
+}
--- leetcode/m0003_longest_substring_without_repeating_chars/longest_substring_without_repeating_test.go
+++ leetcode/m0003_longest_substring_without_repeating_chars/longest_substring_without_repeating_test.go
@@ -1,6 +1,8 @@
 package longestsubstringwithoutrepeatingchars
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_lengthOfLongestSubstring(t *testing.T) {
 	type args struct {
@@ -37,6 +39,53 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
 				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_lengthOfLongestSubstringOptimized(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				s: " ",
+			},
+			want: 1,
+		},
+		{
+			name: "2",
+			args: args{
+				s: "pwwkew",
+			},
+			want: 3,
+		},
+		{
+			name: "3",
+			args: args{
+				s: "bbbbb",
+			},
+			want: 1,
+		},
+		{
+			name: "4",
+			args: args{
+				s: "abba",
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstringOptimized(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstringOptimized() = %v, want %v", got, tt.want)
 			}
 		})
 	}
