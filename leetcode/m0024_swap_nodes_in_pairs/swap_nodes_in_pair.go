44bc93a2ffc3df2d97de4ef12801a6eb708d7d31
Michael Wang <michael.wang@vertiv.com>
2022-12-09 18:03:47 +0800

    remove wrong answer of 24

--- leetcode/m0024_swap_nodes_in_pairs/swap_nodes_in_pair.go
+++ leetcode/m0024_swap_nodes_in_pairs/swap_nodes_in_pair.go
@@ -3,26 +3,5 @@ package swap_nodes_in_pair
 import "practice/data_structure/node"
 
 func swapPairs(head *node.ListNode) *node.ListNode {
-	if head == nil {
-		return nil
-	}
 
-	if head.Next == nil {
-		return head
-	}
-
-	p := head
-	for ; head.Next != nil; head = head.Next {
-		swap(head, head.Next)
-		if head == nil {
-			break
-		}
-	}
-
-	return p
-}
-
-func swap(n1, n2 *node.ListNode) {
-	n1.Next = n2.Next
-	n2.Next = n1
 }
