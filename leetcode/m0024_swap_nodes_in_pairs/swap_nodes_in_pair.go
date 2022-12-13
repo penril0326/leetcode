ace1990eabb3a65d8687818ec9bdad0cb11be881
Michael Wang <michael.wang@vertiv.com>
2022-12-13 17:52:01 +0800

    add iterative solution of 24

--- leetcode/m0024_swap_nodes_in_pairs/swap_nodes_in_pair.go
+++ leetcode/m0024_swap_nodes_in_pairs/swap_nodes_in_pair.go
@@ -2,6 +2,26 @@ package swap_nodes_in_pair
 
 import "practice/data_structure/node"
 
+// Iterative
+// Time: O(N)
+// Space: O(1)
 func swapPairs(head *node.ListNode) *node.ListNode {
+	if (head == nil) || (head.Next == nil) {
+		return head
+	}
 
+	first := new(node.ListNode) // first
+	first.Next = head           // first -> 1 -> 2 -> 3 -> 4
+	prev := first               // first(prev) -> 1 -> 2 -> 3 -> 4
+	for (head != nil) && (head.Next != nil) {
+		keepNext := head.Next.Next // first(prev) -> 1(head) -> 2 -> 3(keepNext) -> 4
+		head.Next.Next = head      // first(prev) -> 1(head) <-> 2  3(keepNext) -> 4
+		prev.Next = head.Next      // first(prev) -> 2 <-> 1(head)  3(keepNext) -> 4 (prev = first)
+		head.Next = keepNext       // first -> 2 -> 1(head) -> 3(keepNext) -> 4
+
+		prev = head     // first -> 2 -> 1(head, prev) -> 3(keepNext) -> 4
+		head = keepNext // first -> 2 -> 1(prev) -> 3(head, keepNext) -> 4
+	}
+
+	return first.Next
 }
