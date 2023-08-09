f36b14b8a1bd7d97b07b186fdf9d106b9228a374
Michael Wang <michael.wang@vertiv.com>
2023-08-09 11:13:21 +0800

    add BFS of 111

--- leetcode/e0111_minimum_depth_of_binary_tree/min_depth_binary_tree.go
+++ leetcode/e0111_minimum_depth_of_binary_tree/min_depth_binary_tree.go
@@ -28,3 +28,39 @@ func min(x, y int) int {
 
 	return y
 }
+
+// BFS
+// Time: O(N)
+// Space: O(N)
+func minDepth2(root *node.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	queue := []*node.TreeNode{}
+	queue = append(queue, root)
+	depth := 1
+	for len(queue) > 0 {
+		size := len(queue)
+		for size > 0 {
+			cur := queue[0]
+			if (cur.Left == nil) && (cur.Right == nil) {
+				return depth
+			}
+
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+
+			size--
+			queue = queue[1:]
+		}
+		depth++
+	}
+
+	return depth
+}
