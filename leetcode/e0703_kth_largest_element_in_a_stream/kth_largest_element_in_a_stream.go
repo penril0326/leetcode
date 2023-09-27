045c106b2498f9f4e6e83ae9139d034fce4cc58a
Michael Wang <michael.wang@vertiv.com>
2023-09-27 18:24:12 +0800

    solution 703

---
+++ leetcode/e0703_kth_largest_element_in_a_stream/kth_largest_element_in_a_stream.go
@@ -0,0 +1,80 @@
+package kthlargestelementinastream
+
+import "container/heap"
+
+type KthLargest struct {
+	vals *myHeap
+	k    int
+}
+
+// O(N*logk), N is size of nums
+func Constructor(k int, nums []int) KthLargest {
+	kth := KthLargest{
+		vals: &myHeap{},
+		k:    k,
+	}
+
+	heap.Init(kth.vals)
+	for _, v := range nums {
+		kth.Add(v)
+	}
+
+	return kth
+}
+
+// O(M*logk), which M is the call times for add()
+func (this *KthLargest) Add(val int) int {
+	// O(logk)
+	heap.Push(this.vals, val)
+
+	if this.vals.Len() > this.k {
+		// O(logk)
+		heap.Pop(this.vals)
+	}
+
+	return this.vals.Top()
+}
+
+type myHeap []int
+
+func (h myHeap) Len() int {
+	return len(h)
+}
+
+func (h myHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h myHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *myHeap) Push(v interface{}) {
+	*h = append(*h, v.(int))
+}
+
+func (h *myHeap) Pop() interface{} {
+	if h.Len() == 0 {
+		panic("Pop no element in heap")
+	}
+
+	top := h.Top()
+	lastIdx := h.Len() - 1
+	*h = (*h)[:lastIdx]
+
+	return top
+}
+
+func (h myHeap) Top() int {
+	if h.Len() == 0 {
+		panic("Top no element in heap")
+	}
+
+	return h[0]
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * obj := Constructor(k, nums);
+ * param_1 := obj.Add(val);
+ */
