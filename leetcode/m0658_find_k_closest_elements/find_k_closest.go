a1b08211c28ea51aa7ed7f0d1d6878e07bf0b094
penril0326 <penril0326@gmail.com>
2023-10-02 23:06:53 +0800

    add heap approach for 658

---
+++ leetcode/m0658_find_k_closest_elements/find_k_closest.go
@@ -0,0 +1,77 @@
+package findkclosestelements
+
+import (
+	"container/heap"
+	"sort"
+)
+
+// Time: O((N+k)*logk), N is size of arr
+// Space: O(k)
+func findClosestElements(arr []int, k int, x int) []int {
+	q := &pq{}
+	for _, v := range arr {
+		temp := [2]int{abs(v - x), v}
+		heap.Push(q, temp)
+
+		if q.Len() > k {
+			heap.Pop(q)
+		}
+	}
+
+	ans := make([]int, k)
+	for i := 0; i < k; i++ {
+		top := heap.Pop(q).([2]int)
+		ans[i] = top[1]
+	}
+
+	sort.Ints(ans)
+
+	return ans
+}
+
+func isClosest(a, b [2]int) bool {
+	if (a[0] < b[0]) || ((a[0] == b[0]) && (a[1] < b[1])) {
+		return true
+	}
+
+	return false
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+// pq[i][0] is distance, pq[i][1] is value
+type pq [][2]int
+
+func (q pq) Len() int {
+	return len(q)
+}
+
+func (q pq) Less(i, j int) bool {
+	return !isClosest(q[i], q[j])
+}
+
+func (q pq) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+
+func (q *pq) Push(v interface{}) {
+	*q = append(*q, v.([2]int))
+}
+
+func (q *pq) Pop() interface{} {
+	if q.Len() == 0 {
+		return nil
+	}
+
+	lastIdx := q.Len() - 1
+	root := (*q)[lastIdx]
+	*q = (*q)[:lastIdx]
+
+	return root
+}
