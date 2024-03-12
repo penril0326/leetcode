38a94cdf11309fbff8d8f39c6ab44423317c3214
penril0326 <penril0326@gmail.com>
2024-03-12 13:13:19 +0800

    add solution of 46

---
+++ leetcode/m0046_permutations/permutations.go
@@ -0,0 +1,35 @@
+package permutations
+
+// Time: O(n^2*n!), n is length of nums
+func permute(nums []int) [][]int {
+	ans := [][]int{}
+	backtracing([]int{}, &ans, nums)
+	return ans
+}
+
+func backtracing(curr []int, ans *[][]int, nums []int) {
+	if len(curr) == len(nums) {
+		new := make([]int, len(curr))
+		copy(new, curr)
+		*ans = append(*ans, new)
+		return
+	}
+
+	for _, n := range nums {
+		if !IsContain(curr, n) {
+			curr = append(curr, n)
+			backtracing(curr, ans, nums)
+			curr = curr[:len(curr)-1]
+		}
+	}
+}
+
+func IsContain(s []int, v int) bool {
+	for _, sv := range s {
+		if sv == v {
+			return true
+		}
+	}
+
+	return false
+}
